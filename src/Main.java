6661fca1c671dc6e20ce37b0d2329f75f328247c
Shakirova Merei <sh.merei05@gmail.com>
2023-05-22 16:59:28 +0600

    commit2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Edge<Vertex> {
+    private Vertex source;
+    private Vertex dest;
+    private Double weight;
+    public Edge(Vertex source, Vertex dest, Double weight){
+        this.source = source;
+        this.dest = dest;
+        this.weight = weight;
     }
 }
