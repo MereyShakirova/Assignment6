2218776d5b78d21b5c188e18937cf1365bfcdcda
Shakirova Merei <sh.merei05@gmail.com>
2023-05-31 23:47:17 +0600

    commit18

--- src/BreadthFirstSearch.java
+++ src/BreadthFirstSearch.java
@@ -1,20 +1,25 @@
-import org.w3c.dom.ls.LSInput;
-
 import java.util.*;
 public class BreadthFirstSearch<V> {
 private WeightedGraph<V> graph;
 public BreadthFirstSearch(WeightedGraph<V> graph){
     this.graph = graph;
 }
-public List<Vertex<V>> BFS(Vertex<V> startVertex){
+public List<Vertex<V>> BFS(Vertex<V> startVertex) {
     List<Vertex<V>> called = new ArrayList<>();
     Queue<Vertex<V>> queue = new LinkedList<>();
     called.add(startVertex);
     queue.offer(startVertex);
-    while(!queue.isEmpty()){
+
+    while (!queue.isEmpty()) {
         Vertex<V> currentNode = queue.poll();
         List<Vertex<V>> nexts = graph.getAdjacentVertices(currentNode);
-        for(Vertex<V>)
+
+        for (Vertex<V> nextNode : nexts) {
+            if (!called.contains(nextNode)) {
+                called.add(nextNode);
+                queue.offer(nextNode);
+            }
+        }
     }
-}
+    return called;
 }
