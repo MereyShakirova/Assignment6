7d3fc2e7553b7ee99ea99ab69b3ee8db277b2820
Shakirova Merei <sh.merei05@gmail.com>
2023-05-31 23:43:07 +0600

    commit17

--- src/BreadthFirstSearch.java
+++ src/BreadthFirstSearch.java
@@ -7,6 +7,14 @@ public BreadthFirstSearch(WeightedGraph<V> graph){
     this.graph = graph;
 }
 public List<Vertex<V>> BFS(Vertex<V> startVertex){
-    List<Vertex>
+    List<Vertex<V>> called = new ArrayList<>();
+    Queue<Vertex<V>> queue = new LinkedList<>();
+    called.add(startVertex);
+    queue.offer(startVertex);
+    while(!queue.isEmpty()){
+        Vertex<V> currentNode = queue.poll();
+        List<Vertex<V>> nexts = graph.getAdjacentVertices(currentNode);
+        for(Vertex<V>)
+    }
 }
 }
