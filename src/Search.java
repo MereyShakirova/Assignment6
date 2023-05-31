b33e2e07c454e8841aaf371f5801f0d83397f5aa
Shakirova Merei <sh.merei05@gmail.com>
2023-05-31 23:10:19 +0600

    commit14

--- src/Search.java
+++ src/Search.java
@@ -1,1 +1,10 @@
+import java.util.*;
+public class Search<V>{
+    private WeightedGraph<V> graph;
+    public Search(WeightedGraph<V> graph){
+        this.graph = graph;
+    }
+    public List<Vertex<V>> BFS(Vertex<V> startVertex){
 
+    }
+}
