7ffd65901997a072e01aaf79447a0d5f9b8442b6
Shakirova Merei <sh.merei05@gmail.com>
2023-06-01 00:42:17 +0600

    commit22

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,18 @@
+public class Main{
+    public static void main(String[] args){
+        WeightedGraph<String> graph = new WeightedGraph<>();
+        Vertex<String> vertex1 = new Vertex<>("Alpha");
+        Vertex<String> vertex2 = new Vertex<>("Beta");
+        Vertex<String> vertex3 = new Vertex<>("Gamma");
+        Vertex<String> vertex4 = new Vertex<>("");
 
+        graph.addVertex(vertex1):
+        graph.addVertex(vertex2);
+        graph.addVertex(vertex3);
+        graph.addVertex(vertex4);
+
+        grap
+
+    }
+}
 
