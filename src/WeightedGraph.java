6caad4b1460d02d6c033c621932b6253c60a24fa
Shakirova Merei <sh.merei05@gmail.com>
2023-05-31 23:06:28 +0600

    commit13

--- src/WeightedGraph.java
+++ src/WeightedGraph.java
@@ -16,4 +16,14 @@ public void addEdge(Vertex<V> source, Vertex<V> destination, double weight) {
     }
 
 }
+public List<Vertex<V>> getAdjacentVertices(Vertex<V> vertex){
+        if(map.containsKey(vertex)){
+            return map.get(vertex);
+        }else{
+            throw new IllegalArgumentException("Vertex not located in this graph.");
+        }
+        public Set<Vertex<V>> getVertices(){
+            return map.keySet();
+    }
+    }
 }
