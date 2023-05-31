83ce5943e9e7dc514b10289a70ea1a1465614b5f
Shakirova Merei <sh.merei05@gmail.com>
2023-05-31 19:58:48 +0600

    commit8

--- src/BreadthFirstSearch.java
+++ src/BreadthFirstSearch.java
@@ -1,2 +1,14 @@
-public class BreadthFirstSearch {
+import java.util.*;
+public class BreadthFirstSearch<V> extends Search<V> {
+    private Set<Vertex<V>> visited;
+    public BreadthFirstSearch(WeightedGraph<V> graph){
+        super(graph);
+        visited = new HashSet<>();
+    }
+    public void BFS(Vertex<V> startVertex){
+        Queque<Vertex<V>> queque = new LinkedList<>();
+        queque.offer(startVertex);
+        visited.add(startVertex);
+        while(!queque)
+    }
 }
