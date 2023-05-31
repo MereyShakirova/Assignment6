21fea5df804010e57c6e5603888a84e061c79a1a
Shakirova Merei <sh.merei05@gmail.com>
2023-05-31 19:43:01 +0600

    commit4

--- src/Vertex.java
+++ src/Vertex.java
@@ -1,3 +1,4 @@
+import java.util. *;
 public class Vertex<V> {
     private V data;
     private Map<Vertex<V>, Double> adjacentVertices;
@@ -7,6 +8,9 @@ public class Vertex<V> {
     }
     public void addAdjacentVertex(Vertex<V> destination, double  weight){
         adjacentVertices.put(destination, weight);
+    }
+    public Map<Vertex<V>, Double> getAdjacentVertices(){
+        return adjacentVertices;
     }
 
 }
